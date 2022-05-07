d2f1a1016943ded18a244ecdf182d9734a9ebbbf
smarist <stmarisnwokike@gmail.com>
2022-05-07 21:10:47 +1200

    made some changes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,31 @@
+import React from 'react'
+
 function Image({className, img}) {
+    const [hovered, setHovered] = React.useState(false)
+
+    function onEnter(){
+        setHovered(true)
+        console.log(hovered)
+    }
+
+    function onLeave(){
+        setHovered(false)
+        console.log(hovered)
+    }
+
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
-        <div className={`${className} image-container`}>
+        <div 
+        className={`${className} image-container`}
+        onMouseLeave={onLeave}
+        onMouseEnter={onEnter}
+        >
             <img src={img.url} className="image-grid" alt="photos"/>
+            {heartIcon}
+            {cartIcon}
+            
         </div>
     )
 }
